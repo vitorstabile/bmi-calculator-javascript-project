45aae53859f9f7e6c1903b98e81ad94781f8ce94
Vitor Garcia <vitor.stabile@hotmail.com>
2021-04-08 17:01:59 -0300

    Test Get Element by ID

---
+++ js/myscript.js
@@ -0,0 +1,18 @@
+
+var weight;
+var height;
+var bmi;
+var result;
+
+
+function calculatorBMI() {
+
+    weight = document.getElementById('weight').value;
+
+    height = document.getElementById('height').value;
+
+    alert(weight);
+    
+    alert(height);
+
+}
